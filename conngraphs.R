9fd0e1b3860740aab52dd3b985cbaa4c4f26efd9
dokato <raymon92@gmail.com>
2015-08-04 19:32:34 +0200

    database after preprocessing added + first try to make graphs dependency plot

---
+++ conngraphs.R
@@ -0,0 +1,49 @@
+library("ggplot2")
+library("dplyr")
+library("igraph")
+
+trim <- function(x) gsub("^\\s+|^\n|\\s+$", "", x)
+
+load("apd.Rda")
+tags.lst<-strsplit(apddata$keypl,",")
+
+# trim(unlist(wx[3]))
+# gsub(".","",unlist(wx[3]))
+# gsub(",","",unlist(wx[3]))
+# tolower(unlist(wx[3]))
+# 
+# dfx <- data.frame(v1= character(0), v2= character(0), strength= numeric(0))
+# 
+# g <- graph.empty()
+# 
+# g["a","b", attr="weight"] <- 2
+# g["c","a"] <- g["c","a"] + 4
+# length(V(g)[V(g)$name=='a' ]) #check if exists
+
+graph.tags <- graph.empty(directed=FALSE)
+for (i in 1:length(tags.lst)){
+  cat(i, " ")
+  row <- trim(unlist(tags.lst[i]))
+  if (sum(is.na(row))){
+    next
+  }
+  row<-gsub(".","",row)
+  row<-gsub(",","",row)
+  row<-tolower(row)
+  
+  for (k in 1:length(row)){
+    if (length(V(graph.tags)[V(graph.tags)$name==row[k]])==0){
+      graph.tags <- graph.tags + vertex(row[k])
+    }
+  }
+  for (k in 1:length(row)){
+    for (j in 1:length(row)){
+      if (graph.tags[row[k], row[j]]==0){
+        graph.tags[row[k], row[j], attr="weight"] <- 1
+      }
+      else{
+        graph.tags[row[k], row[j]] <- graph.tags[row[k], row[j]] + 1
+      }
+    }
+  }
+}
