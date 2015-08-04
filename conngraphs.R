caa01dd5bfb935532d5f4c0f5385811fa39087f0
dokato <raymon92@gmail.com>
2015-08-05 00:20:37 +0200

    added values in graph vertices

--- conngraphs.R
+++ conngraphs.R
@@ -21,19 +21,28 @@ tags.lst<-strsplit(apddata$keypl,",")
 # length(V(g)[V(g)$name=='a' ]) #check if exists
 
 graph.tags <- graph.empty(directed=FALSE)
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# weight of the vertex
+
 for (i in 1:length(tags.lst)){
   cat(i, " ")
   row <- trim(unlist(tags.lst[i]))
   if (sum(is.na(row))){
     next
   }
-  row<-gsub(".","",row)
+  row<-gsub("\\.","",row)
   row<-gsub(",","",row)
   row<-tolower(row)
   
   for (k in 1:length(row)){
     if (length(V(graph.tags)[V(graph.tags)$name==row[k]])==0){
-      graph.tags <- graph.tags + vertex(row[k])
+      graph.tags <- graph.tags + vertex(row[k], val=1)
+    }
+    else{
+      #graph.tags[row[k]] <- graph.tags[row[k]] + 1
+      currval <-get.vertex.attribute(graph.tags,'val',row[k])
+      graph.tags <- set.vertex.attribute(graph.tags,'val',row[k],currval+1)
     }
   }
   for (k in 1:length(row)){
