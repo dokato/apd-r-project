329307eb2eb0a6898022bf690c8f64ad8f4cda10
dokato <raymon92@gmail.com>
2015-10-12 22:15:04 +0200

    shiny plot <nr of diplomas> (Magda's work)

---
+++ nr_of_diplomas/server.R
@@ -0,0 +1,38 @@
+library(shiny)
+
+library("ggplot2")
+library("dplyr")
+load("../apd.Rda")
+dane=apddata
+rm(cit)
+dane$rok=as.numeric(format(dane$data,'%Y'))
+colnames(dane)[6]= "Płeć"
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+  output$myPlot <- renderPlot({
+
+    
+    
+    if (input$select=="dane"){
+    #All
+    ggplot(dane,aes(rok,fill=Płeć))+geom_bar()+
+      xlab("Rok")+ylab("Liczba prac")+
+      scale_fill_brewer(palette="Paired")+
+     theme(axis.title.x = element_text(family="serif", face="bold", size=14, angle=00, hjust=0.54, vjust=0))+
+      theme(axis.title.y = element_text(family="serif", face="bold", size=14))
+    }
+    
+    else{
+    dd=dane%>%
+      filter(field==input$select)
+    ggplot(dd,aes(rok,fill=Płeć))+geom_bar()+
+      xlab("Rok")+ylab("Liczba prac")+
+      scale_fill_brewer(palette="Paired")+
+      theme(axis.title.x = element_text(family="serif", face="bold", size=14))+
+      theme(axis.title.y = element_text(family="serif", face="bold", size=14))
+    }
+    
+  })
+})
