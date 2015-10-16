765aacdd385d81cfae8d3515ffa610e6ae5b6715
dokato <raymon92@gmail.com>
2015-10-16 21:50:31 +0200

    factor of year added

--- nr_of_diplomas/server.R
+++ nr_of_diplomas/server.R
@@ -4,8 +4,9 @@ library("ggplot2")
 library("dplyr")
 load("../apd.Rda")
 dane=apddata
-rm(cit)
+#rm(cit)
 dane$rok=as.numeric(format(dane$data,'%Y'))
+dane <- dane[!is.na(dane$rok),]
 colnames(dane)[6]= "Płeć"
 
 # Define server logic required to draw a histogram
@@ -17,7 +18,7 @@ shinyServer(function(input, output) {
     
     if (input$select=="dane"){
     #All
-    ggplot(dane,aes(rok,fill=Płeć))+geom_bar()+
+    ggplot(dane,aes(factor(rok),fill=Płeć))+geom_bar(binwidth=1)+
       xlab("Rok")+ylab("Liczba prac")+
       scale_fill_brewer(palette="Paired")+
      theme(axis.title.x = element_text(family="serif", face="bold", size=14, angle=00, hjust=0.54, vjust=0))+
@@ -27,7 +28,7 @@ shinyServer(function(input, output) {
     else{
     dd=dane%>%
       filter(field==input$select)
-    ggplot(dd,aes(rok,fill=Płeć))+geom_bar()+
+    ggplot(dd,aes(factor(rok),fill=Płeć))+geom_bar(binwidth=1)+
       xlab("Rok")+ylab("Liczba prac")+
       scale_fill_brewer(palette="Paired")+
       theme(axis.title.x = element_text(family="serif", face="bold", size=14))+
